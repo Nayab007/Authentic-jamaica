597c6546bcb33e5b4cca94e6a7fbe47d2db5f990
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2021-06-25 12:38:39 +0200

    404

--- components/Skeleton.js
+++ components/Skeleton.js
@@ -13,7 +13,7 @@ export default function Skeleton() {
             margin: 20px auto;
           }
           .skeleton > div {
-            background: #bbe5bd;
+            background: #dbcc1a;
             border-radius: 4px;
             margin: 20px 0;
           }
