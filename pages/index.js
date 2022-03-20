74c077b9f9209a1a8aa109b746422a21bb9b885f
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-03-20 14:29:09 +0100

    add

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Recipes({ recipes }) {
       <style jsx>{`
         .recipe-list {
           display: grid;
-          grid-template-columns: 1fr 1fr;
+          grid-template-columns: 1fr 1fr 1fr;
           grid-gap: 20px 60px;
         }
       `}</style>
